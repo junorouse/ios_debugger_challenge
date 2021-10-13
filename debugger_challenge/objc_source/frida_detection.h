f2c63b44340ed8dcc056766d1e54e1ec8cc88575
Juno Im <junorouse@gmail.com>
2021-10-13 14:03:35 +0900

    Patch frida module check

--- debugger_challenge/objc_source/frida_detection.h
+++ debugger_challenge/objc_source/frida_detection.h
@@ -21,17 +21,18 @@
 #define START 26000
 #define END 27500
 #define FRIDA_DEFAULT 27042
-#define MAX_FRIDA_STRINGS 6
+#define MAX_FRIDA_STRINGS 7
 #define MAX_STR_LEN 15
 #define THREAD_NAME_MAX 30
 
-static const char frida_strings[MAX_FRIDA_STRINGS][MAX_STR_LEN] = {
+static const char frida_strings[][MAX_STR_LEN] = {
         { 0x66, 0x72, 0x69, 0x64, 0x61 },// frida
         { 0x66, 0x72, 0x69, 0x64, 0x61, 0x2d, 0x73,0x65,0x72,0x76,0x65,0x72 }, // frida-server
         { 0x46, 0x52, 0x49, 0x44, 0x41 },
         "frida-gadget",
         "gum-js-loop",
-        "gdbus"
+        "gdbus",
+        "FridaGadget"
 };
 
 typedef int (*funcptr)( void );
